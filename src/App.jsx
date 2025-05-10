f40476c2109260a06ff5bfb2885bfa7c277f6c40
Bartue <[bartbatiancila28@gmail.com]>
2025-05-10 11:25:16 +0800

    Edit the imported Links component. as well as the bg-color of the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Links from "./Pages/Links"
+import Links from "./Components/Links"
 import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
     <div className="px-55 py-15 flex justify-between gap-15 ">
-      <div className=" h-120 w-full  rounded-md  bg-blue-950  shadow-xl px-10 py-5">
+      <div className=" h-120 w-full  rounded-md  bg-[#264653]  shadow-xl px-10 py-5">
       < Outlet />
       </div>
       <Links />
