3dc65568889a5802aa4b5a7f08b8706ecc20e329
Bartue <[bartbatiancila28@gmail.com]>
2025-05-10 09:55:46 +0800

    Import createBrowserRouter and RouterProvider from react-router-dom. Used it to invoke the pages from the nav/links component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from './Pages/Error-page.jsx';
+import About from './Pages/About.jsx'
+import Projects from './Pages/Projects.jsx';
+import ContactMe from './Pages/Contact-me.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: < App/>,
+    errorElement: < ErrorPage/>,
+    children: [
+      { index: true, element: < About />},
+      { path: "about", element: < About/> },
+      { path: "projects", element: < Projects/> },
+      { path: "contact-me", element: < ContactMe/> }
+    ]
+  },
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider  router={router}/>
   </StrictMode>,
 )
