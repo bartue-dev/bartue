84c6816a39045dc3853484d9fa0a9d804d819ea9
Bartue <[bartbatiancila28@gmail.com]>
2025-05-10 09:54:12 +0800

    Import Outlet and Link from react-router-dom then invoke it in the App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import Links from "./Pages/Links"
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    <h1>HELLO!</h1>
-    </>
+    <div className="px-55 py-15 flex justify-between gap-15 ">
+      <div className=" h-120 w-full  rounded-md  bg-blue-950  shadow-xl px-10 py-5">
+      < Outlet />
+      </div>
+      <Links />
+    </div>
   )
 }
 
