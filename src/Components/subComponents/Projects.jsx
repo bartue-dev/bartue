89d00fb2a21b65a54ca3b3a31e7886517e4dd2dc
Bartue <[bartbatiancila28@gmail.com]>
2025-06-17 19:11:19 +0800

    Render link data if exist. Add some conditional rendering to make sure it render the need data. Add some comments

--- src/Components/subComponents/Projects.jsx
+++ src/Components/subComponents/Projects.jsx
@@ -15,32 +15,45 @@ function Projects() {
         <div key={i} className=" mb-2">
           <div className="flex items-center gap-2">
 
-            <h1 className="text-pink-600">{item.title}</h1>
+            <h1 className="text-pink-600">{item?.title}</h1>
+
 
             <div className="flex items-center">
               <h1 className="mr-2">-&gt;</h1>
+
+              {/* if link exist then render it */}
+              {item?.link && <a href={item?.link} target="_blank" className="underline underline-offset-2">github</a>}
+
+              {/* if frontend exist then render frontend data */}
               {item.code?.frontend
-              && <a href={item.code?.frontend} className="underline">
+              && <a href={item.code?.frontend} className="underline underline-offset-2">
                   frontend code
                 </a>
               }
-              /
+
+              {/* if backend and frontend exist then render "/" */}
+              {(item?.frontend?.length > 0 && item?.backend?.length > 0) && <h1>/</h1>}
+
+              {/* if backend exist then render its data */}
               {item.code?.backend
-                && <a href={item.code?.backend} className="underline">
+                && <a href={item.code?.backend} className="underline underline-offset-2">
                     backend code
                   </a>
               }
 
+              {/* if status exists then render its data */}
               {item?.status && <h1 className="ml-2 italic"> <span className="mr-2">-&gt;</span>{item.status}</h1>}
 
             </div>
             
           </div>
 
-          <h1>- {item.about}</h1>
+          {item?.about && <h1>- {item?.about}</h1> }
 
           <div>
-            <h1>- built with:</h1>
+            {(item?.backend?.length > 0 || item?.frontend?.length > 0)
+              && <h1>- built with:</h1>}
+
             {item?.frontend
             && <div className="ml-4">
                 {item.frontend.map((front, j) => (
